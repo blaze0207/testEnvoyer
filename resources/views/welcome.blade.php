628725a1c0827b518fbabf0445d762b777a55844
hahn <blaze0207@gmail.com>
2019-04-11 16:44:08 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    Test RRRR
                 </div>
 
                 <div class="links">
