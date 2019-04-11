de390304185b70aab5a4e5ce8c83370c33fb8702
hahn <blaze0207@gmail.com>
2019-04-11 16:47:59 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test RRRR
+                    Test QQQ
                 </div>
 
                 <div class="links">
