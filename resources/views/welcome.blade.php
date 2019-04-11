dc9cf21a44e9a9766afdea6029807bd01a96563a
hahn <blaze0207@gmail.com>
2019-04-11 16:42:48 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test CCC
+                    Test
                 </div>
 
                 <div class="links">
